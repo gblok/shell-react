f692a77890eef90c2fff83fcb9ff1d558f4aecba
gblok <gblok.web@gmail.com>
2017-08-21 18:33:43 +0300

    doc

---
+++ redme.md
@@ -0,0 +1,15 @@
+React-Shell
+
+1 setup
+npm run i
+2 build dll (vendor lib)
+npm run dll
+
+3 build client script
+npm run prod
+
+4 run server-side App (ssr/api)
+npm run nodemon
+
+5 run client-side Dev App webpack-dev-server (on 127.0.0.1:8888) -> depends run server-side for API req on 127.0.0.1:8787
+npm run dev
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import Koa from 'koa'
-import Primus from 'Primus'
 import {API, APP, PORT, STATIC} from '../shared/config/server'
 import middleware from './middleware'
 import routes from './routes'
@@ -7,7 +6,6 @@ import {init} from '../shared/modules/init'
 
 
 const app = new Koa
-const primus = new Primus(app, {transformer: 'uws'})
 
 init()
 
@@ -19,14 +17,17 @@ app
     .listen(PORT, () => console.log(`${String.fromCharCode(9763)} ${PORT}`))
 
 
+//ToDo ws example
 
-//primus.library()
-primus.save(`${STATIC}/assets/js/ws.js`)
-
-primus.on('connection', spark => {
-    console.log('connection')
-})
-
-primus.on('disconnection',  spark =>{
-    console.log('disconnection')
-})
+// const primus = new Primus(app, {transformer: 'uws'})
+// import Primus from 'Primus'
+// //primus.library()
+// primus.save(`${STATIC}/assets/js/ws.js`)
+//
+// primus.on('connection', spark => {
+//     console.log('connection')
+// })
+//
+// primus.on('disconnection',  spark =>{
+//     console.log('disconnection')
+// })
