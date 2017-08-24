fd1b95f64e28d7c93755405bd0dd1f13d99852a6
gblok <gblok.web@gmail.com>
2017-08-24 12:39:31 +0300

    add SW

--- client/index.js
+++ client/index.js
@@ -1,18 +1,27 @@
 import 'babel-polyfill'
-/// import WebFont from 'webfontloader'
 import {h, render} from '../shared/modules'
 import {Shell} from '../shared/components'
-import {googleFont, InitProps, root} from '../shared/config'
+import {InitProps, root} from '../shared/config'
 
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js', {scope: '/'})
+        .then(reg => console.log(`Registration succeeded. Scope is ${reg.scope}`))
+        .catch(err => console.error(`Registration failed with  ${err}`))
+}
+
+render(h(Shell, InitProps), root())
+
+
+/// import WebFont from 'webfontloader'
 // (() => {
 //
 //     WebFont.load({google: googleFont})
 //
 //
 // })()
-
-render(h(Shell, InitProps), root())
-
+//
 // render(h(Shell, InitProps), root())
 //import {InitProps, root} from '../shared/config'
 //import 'url-search-params-polyfill'
---
+++ static/sw.js
@@ -0,0 +1,47 @@
+const PRECACHE = 'v1'
+const RUNTIME = 'runtime'
+
+const PRECACHE_URLS = [
+    './',
+    '/assets/js/vendor.js',
+    '/assets/js/client.js'
+]
+
+
+self.addEventListener('install', e => {
+    console.log('sw install', e)
+
+    e.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    )
+})
+
+
+self.addEventListener('activate', e => {
+    console.log('sw activate', e)
+
+    const currentCaches = [PRECACHE, RUNTIME]
+
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName)))
+            .then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))).then(() => self.clients.claim()))
+    )
+
+})
+
+self.addEventListener('fetch', e => {
+
+    console.log('sw fetch', e)
+
+    if (e.request.url.startsWith(self.location.origin)) {
+        e.respondWith(caches.match(e.request).then(cachedResponse => cachedResponse
+                ? cachedResponse
+                : caches.open(RUNTIME).then(cache => fetch(e.request).then(response => cache.put(e.request, response.clone()).then(() => response)))
+            )
+        )
+    }
+
+})
