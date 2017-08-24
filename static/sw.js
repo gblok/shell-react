b3dcb8b1539bad62ea78de1fa25d2d8a1eca9478
gblok <gblok.web@gmail.com>
2017-08-24 13:28:31 +0300

    add SW

--- static/sw.js
+++ static/sw.js
@@ -1,24 +1,25 @@
-const PRECACHE = 'v1'
-const RUNTIME = 'runtime'
-
-const PRECACHE_URLS = [
-    './',
-    '/assets/js/vendor.js',
-    '/assets/js/client.js'
-]
+const
+    PRECACHE = 'v1',
+    RUNTIME = 'runtime',
+    PRECACHE_URLS = [
+        '/',
+        '/offline.html',
+        '/assets/css/default.css',
+        '/assets/js/vendor.js',
+        '/assets/js/client.js'
+    ]
 
 
 self.addEventListener('install', e => {
     console.log('sw install', e)
 
     e.waitUntil(
-        caches.open(PRECACHE)
+        caches
+            .open(PRECACHE)
             .then(cache => cache.addAll(PRECACHE_URLS))
             .then(self.skipWaiting())
     )
 })
-
-
 self.addEventListener('activate', e => {
     console.log('sw activate', e)
 
@@ -27,7 +28,9 @@ self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys()
             .then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName)))
-            .then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))).then(() => self.clients.claim()))
+            .then(cachesToDelete => Promise
+                .all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete)))
+                .then(() => self.clients.claim()))
     )
 
 })
@@ -37,10 +40,18 @@ self.addEventListener('fetch', e => {
     console.log('sw fetch', e)
 
     if (e.request.url.startsWith(self.location.origin)) {
-        e.respondWith(caches.match(e.request).then(cachedResponse => cachedResponse
-                ? cachedResponse
-                : caches.open(RUNTIME).then(cache => fetch(e.request).then(response => cache.put(e.request, response.clone()).then(() => response)))
-            )
+        e.respondWith(
+            caches
+                .match(e.request)
+                .then(cachedResponse => cachedResponse
+                    ? cachedResponse
+                    : caches
+                        .open(RUNTIME)
+                        .then(cache => fetch(e.request)
+                            .then(response => cache
+                                .put(e.request, response.clone())
+                                .then(() => response)))
+                )
         )
     }
 
