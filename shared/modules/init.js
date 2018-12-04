759f8d2dd3fdde66bbe8ffc796d6256cbb019454
diz <diz.dozer@gmail.com>
2018-12-04 07:52:34 +0200

    WIP

--- shared/modules/init.js
+++ shared/modules/init.js
@@ -6,17 +6,15 @@ import * as Schemes from '../schemes'
 import * as PreloadData from '../data'
 
 
-
-
 export const init = () => {
 
 
-    console.log('init', {IS_CLIENT})
+    // console.log('init', {IS_CLIENT})
 
     filterEsModule(PreloadData)
         .forEach(cid => {
 
-            if(['pages', 'products'].includes(cid))
+            if (['pages', 'products'].includes(cid))
                 handler({cid}, PreloadData[cid])
             console.log({cid})
 
@@ -26,7 +24,7 @@ export const init = () => {
         .forEach(id => handler({cid: 'schemes'}, {id, ...Schemes[id]}))
 
 
-    console.log('__init', {IS_CLIENT})
+    // console.log('__init', {IS_CLIENT})
 
     return IS_CLIENT ? router.start() : false
 }
